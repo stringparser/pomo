1847e31e78e389706437f05ae2e2a6826f72b727
Javier Carrillo Milla <stringparser@gmail.com>
2021-11-13 19:52:27 +0100

    remove env for now

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const offline = require('next-offline');
 
 const nextConfig = {
   // Exposes Server ENV Vars To Client Using Webpack
-  env: {
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  },
+  // env: {
+  //   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+  //   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  // },
   target: process.env.NODE_ENV !== 'production' ? 'server' : 'serverless',
   devSwSrc: './public/sw.js',
   generateSw: false,
